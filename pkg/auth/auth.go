6f8421f0d4ee6e2545aef3a03cbc4e20a816894b
jinsoo-youn <jinsoo_youn@tmax.co.kr>
2021-03-15 23:13:16 +0900

    [feat] ADD promethues proxy

---
+++ pkg/auth/auth.go
@@ -0,0 +1,8 @@
+package auth
+
+// User holds fields representing a user.
+type User struct {
+	ID       string
+	Username string
+	Token    string
+}
--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -1,15 +1,19 @@
 package server
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
 
 	v1 "console/pkg/api/v1"
+	"console/pkg/auth"
 	"console/pkg/hypercloud/proxy"
+	"console/pkg/terminal"
 
 	"github.com/sirupsen/logrus"
 )
@@ -68,7 +72,7 @@ type jsGlobals struct {
 type Router struct {
 	BaseURL    *url.URL
 	PublicDir  string
-	StaticUser *User
+	StaticUser *auth.User
 	// A lister for resource listing a particular kind
 	GOARCH string
 	GOOS   string
@@ -84,6 +88,7 @@ type Router struct {
 	KeycloakClientId string
 	// Proxy
 	K8sProxyConfig                   *proxy.Config
+	TerminalProxyTLSConfig           *tls.Config
 	PrometheusProxyConfig            *proxy.Config
 	ThanosProxyConfig                *proxy.Config
 	ThanosTenancyProxyConfig         *proxy.Config
@@ -110,21 +115,133 @@ func New(config *v1.Config) (*Router, error) {
 
 func (router *Router) Start() http.Handler {
 	standardMiddleware := alice.New(router.recoverPanic, router.logRequest, secureHeaders)
-	tokenMiddleware := alice.New(router.tokenHandler)
+	// tokenMiddleware := alice.New(router.tokenHandler)
 
 	gmux := mux.NewRouter()
 
+	handle := func(path string, handler http.Handler) {
+		gmux.Handle(proxy.SingleJoiningSlash(router.BaseURL.Path, path), handler)
+	}
+	handleFunc := func(path string, handler http.HandlerFunc) { handle(path, handler) }
+
+	authHandlerWithUser := func(hf func(*auth.User, http.ResponseWriter, *http.Request)) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			// if s.StaticUser.Username == "hypercloud" {
+			if router.ReleaseModeFlag {
+				token := r.Header.Clone().Get("Authorization")
+				temp := strings.Split(token, "Bearer ")
+				if len(temp) > 1 {
+					token = temp[1]
+				} else {
+					token = temp[0]
+				}
+				// plog.Infof("check token is on : %v", token)
+				router.StaticUser.Token = token
+
+				// NOTE: query에 token 정보가 있을 시 해당 token으로 설정
+				queryToken := r.URL.Query().Get("token")
+				if queryToken != "" && token == "" {
+					r.URL.Query().Del("token")
+					router.StaticUser.Token = queryToken
+				}
+
+			}
+
+			hf(router.StaticUser, w, r)
+		})
+	}
+
 	k8sProxy := proxy.NewProxy(router.K8sProxyConfig)
 	gmux.PathPrefix(k8sProxyEndpoint).Handler(
-		http.StripPrefix(singleJoiningSlash(router.BaseURL.Path, k8sProxyEndpoint), tokenMiddleware.ThenFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.StripPrefix(singleJoiningSlash(router.BaseURL.Path, k8sProxyEndpoint), authHandlerWithUser(func(user *auth.User, w http.ResponseWriter, r *http.Request) {
 			r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", router.StaticUser.Token))
 			k8sProxy.ServeHTTP(w, r)
 		})),
 	)
+
+	terminalProxy := terminal.NewProxy(
+		router.TerminalProxyTLSConfig,
+		router.K8sProxyConfig.TLSClientConfig,
+		router.K8sProxyConfig.Endpoint)
+
+	handle(terminal.ProxyEndpoint, authHandlerWithUser(terminalProxy.HandleProxy))
+	handleFunc(terminal.AvailableEndpoint, terminalProxy.HandleProxyEnabled)
+
+	if router.prometheusProxyEnabled() {
+		// Only proxy requests to the Prometheus API, not the UI.
+		var (
+			labelSourcePath      = prometheusProxyEndpoint + "/api/v1/label/"
+			rulesSourcePath      = prometheusProxyEndpoint + "/api/v1/rules"
+			querySourcePath      = prometheusProxyEndpoint + "/api/v1/query"
+			queryRangeSourcePath = prometheusProxyEndpoint + "/api/v1/query_range"
+			targetAPIPath        = prometheusProxyEndpoint + "/api/"
+
+			tenancyQuerySourcePath      = prometheusTenancyProxyEndpoint + "/api/v1/query"
+			tenancyQueryRangeSourcePath = prometheusTenancyProxyEndpoint + "/api/v1/query_range"
+			tenancyTargetAPIPath        = prometheusTenancyProxyEndpoint + "/api/"
+
+			prometheusProxy    = proxy.NewProxy(router.PrometheusProxyConfig)
+			thanosProxy        = proxy.NewProxy(router.ThanosProxyConfig)
+			thanosTenancyProxy = proxy.NewProxy(router.ThanosTenancyProxyConfig)
+		)
+
+		// global label, query, and query_range requests have to be proxied via thanos
+		handle(querySourcePath, http.StripPrefix(
+			proxy.SingleJoiningSlash(router.BaseURL.Path, targetAPIPath),
+			authHandlerWithUser(func(user *auth.User, w http.ResponseWriter, r *http.Request) {
+				r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", user.Token))
+				thanosProxy.ServeHTTP(w, r)
+			})),
+		)
+		handle(queryRangeSourcePath, http.StripPrefix(
+			proxy.SingleJoiningSlash(router.BaseURL.Path, targetAPIPath),
+			authHandlerWithUser(func(user *auth.User, w http.ResponseWriter, r *http.Request) {
+				r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", user.Token))
+				thanosProxy.ServeHTTP(w, r)
+			})),
+		)
+		handle(labelSourcePath, http.StripPrefix(
+			proxy.SingleJoiningSlash(router.BaseURL.Path, targetAPIPath),
+			authHandlerWithUser(func(user *auth.User, w http.ResponseWriter, r *http.Request) {
+				r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", user.Token))
+				thanosProxy.ServeHTTP(w, r)
+			})),
+		)
+
+		// alerting (rules) have to be proxied via cluster monitoring prometheus
+		handle(rulesSourcePath, http.StripPrefix(
+			proxy.SingleJoiningSlash(router.BaseURL.Path, targetAPIPath),
+			authHandlerWithUser(func(user *auth.User, w http.ResponseWriter, r *http.Request) {
+				r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", user.Token))
+				prometheusProxy.ServeHTTP(w, r)
+			})),
+		)
+
+		// tenancy queries and query ranges have to be proxied via thanos
+		handle(tenancyQuerySourcePath, http.StripPrefix(
+			proxy.SingleJoiningSlash(router.BaseURL.Path, tenancyTargetAPIPath),
+			authHandlerWithUser(func(user *auth.User, w http.ResponseWriter, r *http.Request) {
+				r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", user.Token))
+				thanosTenancyProxy.ServeHTTP(w, r)
+			})),
+		)
+		handle(tenancyQueryRangeSourcePath, http.StripPrefix(
+			proxy.SingleJoiningSlash(router.BaseURL.Path, tenancyTargetAPIPath),
+			authHandlerWithUser(func(user *auth.User, w http.ResponseWriter, r *http.Request) {
+				r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", user.Token))
+				thanosTenancyProxy.ServeHTTP(w, r)
+			})),
+		)
+	}
 
 	fileServer := http.FileServer(http.Dir(router.PublicDir))
 	gmux.PathPrefix("/static/").Handler(http.StripPrefix(singleJoiningSlash(router.BaseURL.Path, "/static/"), gzipHandler(fileServer)))
 
 	return standardMiddleware.Then(gmux)
 
+}
+
+func (router *Router) prometheusProxyEnabled() bool {
+	return router.PrometheusProxyConfig != nil && router.ThanosTenancyProxyConfig != nil
 }
